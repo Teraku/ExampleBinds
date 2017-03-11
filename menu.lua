00d69fd1e14cb3bad5c33ea124c1a1fbea6e22aa
Hugo Zink <hugo.zink@hotmail.com>
2017-03-11 19:54:30 +0100

    Fixed callbacks not triggering. Proof of concept complete.

--- menu.lua
+++ menu.lua
@@ -3,17 +3,31 @@ local modPath = ModPath
 
 Hooks:Add("MenuManagerInitialize", "ExampleBinds_InitCallbacks", function(menu_manager)
 	
-	MenuCallbackHandler.test_callback = function(self, item)
+	log("[ExampleBinds]Adding menu callbacks")
+	
+	MenuCallbackHandler.test_callback = function()
 		
 		log("A key was pressed!")
 		
 	end
 	
+	MenuCallbackHandler.example_test1 = function()
+		
+		log("Another key was pressed!")
+		
+	end
+	
+	log("[ExampleBinds]Finished adding menu callbacks")
+	
+	log("[ExampleBinds]Building a menu from JSON")
 	MenuHelper:LoadFromJsonFile(modPath .. "menu.json", {}, {})
+	log("[ExampleBinds]Finished building a menu from JSON")
 end)
 
 Hooks:Add("MenuManagerPopulateCustomMenus", "ExampleBinds_PopulateMenu", function(menu_manager, nodes)
 
+		log("[ExampleBinds]Adding keybinds to menu")
+		
 		for i, id in pairs(tests) do
 			MenuHelper:AddKeybinding({
 				id = "example_" .. id,
@@ -25,6 +39,9 @@ Hooks:Add("MenuManagerPopulateCustomMenus", "ExampleBinds_PopulateMenu", functio
 				menu_id = "examplebinds_menu",
 				localized = false
 			})
+			
+			LuaModManager:AddKeybinding("example_" .. id, MenuCallbackHandler.test_callback)
 		end
-
+		
+		log("[ExampleBinds]Finished adding keybinds to menu")
 end)
